87669a6cedfc63b7aa4494965b6a63eb24ca83f6
Stefan.Kohlbrecher@gmail.com <Stefan.Kohlbrecher@gmail.com@750c941b-22bf-78a4-31e1-aa233780f62c>
2012-12-16 19:17:45 +0000

    [rqt_gui_rescue_control_plugin] -add pushbutton for adding victim in front of the robot

--- rqt_gui_rescue_control_plugin/lib/RescueControlDialog.py
+++ rqt_gui_rescue_control_plugin/lib/RescueControlDialog.py
@@ -9,6 +9,8 @@ roslib.load_manifest('rqt_gui_rescue_control_plugin')
 import rospy
 
 from std_msgs.msg import String
+from worldmodel_msgs.srv import AddObject
+from worldmodel_msgs.msg import ObjectState
 
 class RescueControlDialog(QObject):
 
@@ -30,6 +32,12 @@ class RescueControlDialog(QObject):
         self._syscommand_pub = rospy.Publisher('syscommand', String)
         vbox.addWidget(resetPushButton)
         
+        #Add a Pushbutton for adding a victim in front of the robot and projecting it against the wall
+        addVictimInFrontOfRobotPushButton = QPushButton("Add victim in front of robot")       
+        addVictimInFrontOfRobotPushButton.pressed.connect(self.on_add_victim_in_front_of_robot_pressed)
+        vbox.addWidget(addVictimInFrontOfRobotPushButton)
+        
+        
         #add stretch at end so all GUI elements are at top of dialog
         vbox.addStretch(1)
                   
@@ -42,6 +50,40 @@ class RescueControlDialog(QObject):
     def handleReset(self):
         tmp = String("reset")
         self._syscommand_pub.publish(tmp)
+        
+        
+    @Slot()
+    def on_add_victim_in_front_of_robot_pressed(self):
+            
+        try:
+            add_victim = rospy.ServiceProxy('worldmodel/add_object', AddObject)
+            
+            req = AddObject._request_class()
+            req.object.header.frame_id = 'base_link'
+            req.object.header.stamp = rospy.Time(0)
+            req.map_to_next_obstacle = True
+            req.object.info.class_id = "victim"
+            req.object.info.support = 100
+            req.object.pose.pose.position.x = 0.1
+            req.object.pose.pose.position.y = 0
+            req.object.pose.pose.position.z = 0
+            req.object.pose.pose.orientation.w = 1
+            req.object.state.state = ObjectState.CONFIRMED
+
+            resp = add_victim(req)
+            
+            status_msg = "added Victim, id = "
+            status_msg += resp.object.info.object_id
+            self._write_message(status_msg)
+            
+            self._task_id = resp.object.info.object_id
+            self._victimAnswer = VictimAnswer.CONFIRM
+            self._publish_answer()
+            
+        except rospy.ServiceException, e:
+            err_msg = "error: "
+            err_msg += str(e)
+            self._write_message(err_msg)     
         
         
     #Stuff below needed for rqt_gui, do not edit (unless you know what you're doing)           
